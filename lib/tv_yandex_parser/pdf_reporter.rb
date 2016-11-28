b3cd59a360ff9984d7d6a8c0f41531f4ba5697d5
j2FunOnly <j2funonly@ya.ru>
2016-11-28 08:59:12 +0300

    refactor pdf_reporter

--- lib/tv_yandex_parser/pdf_reporter.rb
+++ lib/tv_yandex_parser/pdf_reporter.rb
@@ -5,6 +5,19 @@ module TvYandexParser
     include Prawn::View
 
     def initialize(schedule)
+      @schedule = schedule
+      setup_font
+      render_schedule
+    end
+
+    def to_file(filename)
+      filename += '.pdf' unless filename.end_with? '.pdf'
+      save_as filename
+    end
+
+    private
+
+    def setup_font
       font_families.update(
         'PT Sans' => {
           normal: File.expand_path('../../fonts/pt_sans.ttf', __FILE__),
@@ -13,25 +26,30 @@ module TvYandexParser
       )
       font 'PT Sans'
       font_size 14
+    end
 
-      schedule.each_with_index do |day, i|
+    def render_schedule
+      @schedule.each_with_index do |day, i|
         start_new_page if i > 0
-        text "<font size='16'><b>#{day.title}</b></font>", inline_format: true
-        stroke_horizontal_rule
-        move_down 10
-        if day.available?
-          day.items.each do |item|
-            text "#{item[:time]} #{item[:description]}"
-          end
-        else
-          text day.tv_splash
-        end
+        render_title day.title
+        render_day day
       end
     end
 
-    def to_file(filename)
-      filename += '.pdf' unless filename.end_with? '.pdf'
-      save_as filename
+    def render_title(title)
+      text "<font size='16'><b>#{title}</b></font>", inline_format: true
+      stroke_horizontal_rule
+      move_down 10
+    end
+
+    def render_day(day)
+      if day.available?
+        day.items.each do |item|
+          text "#{item[:time]} #{item[:description]}"
+        end
+      else
+        text day.tv_splash
+      end
     end
   end
 end
